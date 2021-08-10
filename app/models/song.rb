8eb89a2b27b932ff58d6c13c85f6f2be6b72d317
Drew Keat <thedrewkeat@gmail.com>
2021-08-09 22:38:41 -0600

    establish relationships

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,22 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist.find_or_create_by!(name)
+    
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def note_contents=(contents)
+    self.notes.map {|note| note.content}
+  end
+
+  def note_contents
+
+  end
 end
